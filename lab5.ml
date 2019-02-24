70fb2fe92c25dbbcf3f42eada96aa30c74efee4e
Diego Cachay <diegocachaay@college.harvard.edu>
2019-02-24 16:16:55 -0500

    labfinal

--- lab5.ml
+++ lab5.ml
@@ -185,8 +185,8 @@ let convert_to_rgb (col : color) : int * int * int =
     | Simple x -> 
         match x with
         | Red -> (255, 0, 0)
-        | Green -> (0, 64, 0)
-        | Blue -> (0, 255, 255)
+        | Green -> (0, 255, 0)
+        | Blue -> (0, 0, 255)
         | Crimson -> (164, 16, 52)
         | Orange -> (255, 165, 0)
         | Yellow -> (255, 255, 0)
